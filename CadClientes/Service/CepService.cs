e5aa200128adfef57ec5cc73010f563f7fe537f3
matheushoske <matheushoske@gmail.com>
2022-04-06 23:59:36 -0300

    SOLID

---
+++ CadClientes/Service/CepService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using RestSharp.Serialization.Json;
+using System.Net;
+using RestSharp;
+using CadClientes.DAO;
+using System.Text.Json;
+using CadClientes.Interfaces;
+namespace CadClientes
+{
+    public class ServiceCep : ICepService
+    {
+        
+        private readonly ICepRepository _cepRepository;
+        public ServiceCep(ICepRepository cepRepository)
+        {
+            _cepRepository = cepRepository;
+        }
+        public Cep BuscarCep(string cep)
+        {
+            try
+            {
+                return _cepRepository.BuscarCep(cep);
+            }
+            catch 
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ CadClientes/Service/ClienteService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CadClientes.Model;
+using System.Net.Http;
+using System.Text.Json;
+//using Newtonsoft.Json;
+using RestSharp;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http.Headers;
+using CadClientes.Interfaces;
+//using RestSharp.Serialization.Json;
+//using Newtonsoft.Json.Serialization;
+
+namespace CadClientes.Service
+{
+    class ClienteService : IClienteService
+    {
+
+        private readonly IClienteRepository _clienteRepository;
+        public ClienteService(IClienteRepository clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+        public bool AdicionarCliente(Cliente cli)
+        {
+            try
+            {
+                _clienteRepository.AdicionarCliente(cli);
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+
+        public Cliente BuscarCliente(int id)
+        {
+            Cliente retorno = null;
+            try
+            {
+                retorno = _clienteRepository.BuscarCliente(id);
+                return retorno;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+
+        public int RetornaUltimoClienteid(int somar = 0)
+        {
+            try
+            {
+                Cliente cli = _clienteRepository.BuscarUltimoCliente();
+                return cli.id + somar;
+            }
+            catch (Exception)
+            {
+
+                return -1;
+            }
+            
+           
+        }
+        public bool TestarComunicacao() 
+        {
+            if (_clienteRepository.TestarApi())
+                return true;
+            else
+                return false;
+        }
+            
+    }
+
+    }
+    
+
